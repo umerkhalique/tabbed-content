37705c87157aaa6dc6b80789fbd9f33fa347cab5
umer khalique <omerkhalique13@gmail.com>
2022-03-19 19:07:09 +0500

    update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const tabs = document.querySelectorAll(".tab");
 const tab1 = document.querySelector(".tab1");
 const tab2 = document.querySelector(".tab2");
 const tab3 = document.querySelector(".tab3");
@@ -7,17 +6,17 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     if (e.target.textContent === "tab1") {
-      tab1.classList.add('active');
-      tab2.classList.remove('active');
-    tab3.classList.remove('active');
+      tab1.classList.add("active");
+      tab2.classList.remove("active");
+      tab3.classList.remove("active");
     } else if (e.target.textContent === "tab2") {
-      tab2.classList.add('active');
-      tab1.classList.remove('active');
-        tab3.classList.remove('active');
+      tab2.classList.add("active");
+      tab1.classList.remove("active");
+      tab3.classList.remove("active");
     } else {
-      tab3.classList.add('active');
-      tab2.classList.remove('active');
-        tab1.classList.remove('active');
+      tab3.classList.add("active");
+      tab2.classList.remove("active");
+      tab1.classList.remove("active");
     }
   });
 });
